559db7a349bd6cf718fdb2e61e2f28debc98d9c7
hutaobo <hutaobo@bonc.com.cn>
2017-12-14 19:22:14 +0800

    添加消息总线rabbitmq

--- config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.forezp.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -15,7 +17,7 @@ public class ConfigClientApplication {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-    @Value("${foo}")
+    @Value("${server.port}")
     String foo;
 
     @RequestMapping("hi")
